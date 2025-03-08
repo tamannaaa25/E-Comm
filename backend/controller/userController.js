e2089a1357f429d0c7ea80bbf15d860c15787bd0
tamannaaa25 <tamanna.agy@gmail.com>
2025-03-08 22:37:40 +0530

    Add files via upload

---
+++ backend/controller/userController.js
@@ -0,0 +1,20 @@
+
+
+
+//user login
+const loginUser = async (req, res) => {
+    
+};
+
+//user registration
+const registerUser = async (req, res) => {
+    res.json({msg:"User registered successfully"})
+}
+
+
+//admin login
+const adminLogin = async (req, res) => {
+    
+};
+
+export { loginUser, registerUser , adminLogin};
